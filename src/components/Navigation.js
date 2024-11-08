873e976efb08cca811a4a8f9d9866836cf0c97db
zengkaimin <zengkaimin1991@gmail.com>
2024-11-08 23:09:52 +0800

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -57,7 +57,7 @@ export function Navigation() {
       <div className="container flex h-16 items-center justify-between">
         <div className="flex gap-6 md:gap-10">
           <Link href="/" className="flex items-center space-x-2">
-            <span className="inline-block font-bold">GitBase</span>
+            <span className="inline-block font-bold">Chinamake</span>
           </Link>
           <nav className="hidden md:flex gap-6">
             {navItems.map((item) => (
